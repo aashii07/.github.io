da35e05cad247374afd48d4d251f822a8b3899ed
Aashi Jaulim <aashi.jaulim@gmail.com>
2023-02-21 16:40:05 +0400

    dftd

--- js/home.js
+++ js/home.js
@@ -1,13 +1,16 @@
-function switchMode() {
-    let moon = document.getElementById ("moon");
-    if(moon.className=="moon"){
-    moon.className="sun";
-    document.body.style.backgroundColor = "#141D26";
-    document.body.style.color = "#fff";
-    }
-    else {
-    moon.className ="moon";
-    document.body.style.backgroundColor = "#fff";
-    document.body.style.color = "#000";
-    }
-}
+/*const setTheme = theme => 
+document.documentElement.className = theme;*/
+
+const setTheme = theme => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme); // save the theme preference to local storage
+  }
+  
+  // retrieve the theme preference from local storage on page load
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme) {
+    setTheme(savedTheme);
+  }
+
+  
+ 
--- js/joinus.js
+++ js/joinus.js
@@ -1,18 +1,43 @@
 function validateform() {
-    var name = document.myform.name.value;
-    var password = document.myform.password.value;
+    var fname = document.myform.fname.value;
+    var lname = document.myform.lname.value;
+    var phone = document.getElementById("phone");
     var x = document.myform.email.value;
     var atposition = x.indexOf("@");
     var dotposition = x.lastIndexOf(".");
-    if (name == null || name == "") {
-        alert("Name can't be blank");
-        return false;
-    } else if (password.length < 6) {
-        alert("Password must be at least 6 characters long.");
+    var address = document.myform.address.value;
+    if (fname == null || fname == "") {
+        alert("First Name can't be blank");
         return false;
     }
+    if (lname == null || lname == "") {
+        alert("Last Name can't be blank");
+        return false;
+    }
+    phone.addEventListener('input', () => {
+        phone.setCustomValidity('');
+        phone.checkValidity();
+    });
     if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
         alert("Please enter a valid e-mail address");
         return false;
     }
-} 
+    if (address == null || address == "") {
+        alert("Address can't be blank");
+        return false;
+    }
+    // get the checkbox element from the DOM using the getElementId function
+    var checkbox = document.getElementById("Checkbox");
+    // get the text element to display the status of the checkbox
+    var text = document.getElementById("confirm");
+    // use the checked property to check if the checkbox is checked
+    if (checkbox.checked) {
+        // display result by assigning to innerHTML of the text element.
+        text.innerHTML = "Thank you for accepting the agreement";
+    }
+    else {
+        text.innerHTML = "Please accept the agreement to proceed";
+    }
+
+}
+
